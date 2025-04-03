204bf2065c262f7a4ddba756779c3d0d9c2427a6
developers <developers@ibstec.com>
2025-04-03 04:06:01 -0700

    upload new project

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'auth0/login' => 
+  array (
+    'aliases' => 
+    array (
+      'Auth0' => 'Auth0\\Laravel\\Facade\\Auth0',
+    ),
+    'providers' => 
+    array (
+      0 => 'Auth0\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
